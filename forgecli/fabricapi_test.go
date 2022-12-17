b3f56dd33e8909f30a66f5706be8dc97e7ba00a5
christopher blodgett <christopher.blodgett@gmail.com>
2022-12-17 10:34:45 -0800

    Adding mock java calls, validated with binary install tests

--- forgecli/fabricapi_test.go
+++ forgecli/fabricapi_test.go
@@ -59,23 +59,32 @@ func TestFabricClientDownload(t *testing.T) {
 
 // TODO: Figure out how to mock 'java' call
 // BREAKS: on CI/CD in Github
-// func TestFabricClientInstaller(t *testing.T) {
-// 	defer gock.Off()
-// 	MockFabricXML(t)
-// 	MockFabricJAR(t)
+func TestFabricClientInstaller(t *testing.T) {
+	javaInstallFabric = func(installCommands []string) ([]byte, error) {
+		return []byte("mock jar install"), nil
+	}
+	defer gock.Off()
+	MockFabricXML(t)
+	MockFabricJAR(t)
 
-// 	var buf bytes.Buffer
-// 	logrus.SetOutput(&buf)
-// 	var app appEnv
-// 	if err := app.FabricClientInstaller(); err != nil {
-// 		t.Errorf("Test failed, expected: '%s', got:  '%s'", "nil", err)
-// 	}
+	var buf bytes.Buffer
+	logrus.SetOutput(&buf)
+	var app appEnv
+	if err := app.FabricClientInstaller(); err != nil {
+		t.Errorf("Test failed, expected: '%s', got:  '%s'", "nil", err)
+	}
+	rawOutput := strings.Trim(buf.String(), "\n")
+	t.Log(rawOutput)
 
-// 	// Validates logging during the client download
-// 	expected := "Removing file: ./fabric-installer-0.11.1.jar"
-// 	rawOutput := strings.Trim(buf.String(), "\n")
-// 	output := rawOutput[strings.LastIndex(rawOutput, "=")+1:]
-// 	if !strings.Contains(output, expected) {
-// 		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, output)
-// 	}
-// }
+	// Validates Installation of Jar
+	expectedJarInstall := "Install Output: mock jar install"
+	if !strings.Contains(rawOutput, expectedJarInstall) {
+		t.Errorf("Test failed, expected: '%s', got:  '%s'", expectedJarInstall, rawOutput)
+	}
+
+	// Validates remove of Jar
+	expectedRemovalOutput := "Removing file: ./fabric-installer-0.11.1.jar"
+	if !strings.Contains(rawOutput, expectedRemovalOutput) {
+		t.Errorf("Test failed, expected: '%s', got:  '%s'", expectedRemovalOutput, rawOutput)
+	}
+}
